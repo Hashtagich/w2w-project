094b5f828b5a45d956f0f51318383a130d5c8c64
Mariya Kochanova <balalum@gmail.com>
2024-06-18 20:34:20 +0300

    добавила секцию частые вопросы

--- frontend/src/Pages/Home-page/index.jsx
+++ frontend/src/Pages/Home-page/index.jsx
@@ -2,7 +2,8 @@ import Tariffs from './Components/Tariffs/Tariffs';
 import Main from './Components/Main/Main';
 import Section2 from './Components/Section 2/Section2';
 import Section3 from './Components/Section 3/Section3';
-import style1 from './sectors.module.scss'
+import Accordion from './Components/Section 6/Section6';
+import style1 from './sectors.module.scss';
 
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
            <Section2/>
            <Section3/>
            <Tariffs/>
+           <Accordion/>
         </div>
     )
 }
