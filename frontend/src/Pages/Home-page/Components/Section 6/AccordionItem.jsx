869e617f4b8d758313631291ea5a93588d3154ed
Mariya Kochanova <balalum@gmail.com>
2024-06-24 23:55:41 +0300

    финальные стили для лендинга

--- frontend/src/Pages/Home-page/Components/Section 6/AccordionItem.jsx
+++ frontend/src/Pages/Home-page/Components/Section 6/AccordionItem.jsx
@@ -2,29 +2,6 @@ import React, { useRef } from "react";
 import { ReactComponent as ArrowIcon } from "./Vector.svg";
 import style from "./section6.module.scss";
 
-// export const AccordionItem = ({ faqItem, onClick, isOpen }) => {
-//   const itemRef = useRef(null);
-
-//   return (
-//     <li className="accordion-item">
-//       <button className="accordion-header" onClick={() => onClick()}>
-//         {faqItem.q}
-//         <ArrowIcon className={`accordion-arrow ${isOpen ? "active" : ""}`} />
-//       </button>
-//       <div
-//         className="accordion-collapse"
-//         style={
-//           isOpen ? { height: itemRef.current.scrollHeight } : { height: "0px" }
-//         }
-//       >
-//         <div className="accordion-body" ref={itemRef}>
-//           {faqItem.a}
-//         </div>
-//       </div>
-//     </li>
-//   );
-// };
-
 export const AccordionItem = ({ faqItem, onClick, isOpen }) => {
   const itemRef = useRef(null);
 
@@ -32,7 +9,7 @@ export const AccordionItem = ({ faqItem, onClick, isOpen }) => {
     <li className={style["accordion-item"]}>
       <button className={style["accordion-header"]} onClick={() => onClick()}>
         {faqItem.q}
-        <ArrowIcon className={`${style["accordion-arrow"]} ${isOpen ? style["active"] : ""}`} />
+        <ArrowIcon className={`${style["accordion-x"]} ${isOpen ? style["active"] : ""}`} />
       </button>
       <div
         className={style["accordion-collapse"]}
